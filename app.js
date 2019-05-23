4523fb2561e7f1550e174ed3d58dfb0fa93b9fcb
Steven Whitton <stevenawhitton@gmail.com>
2019-05-23 04:19:54 +0200

    Created individual card templates

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ axios
   .get('https://api.pokemontcg.io/v1/cards?pageSize=250&setCode=sm7')
   .then((res, req) => {
     data = res.data
+    routeCards()
   })
   .catch(err => {
     console.log(err)
@@ -23,6 +24,19 @@ axios
 app.get('/', (req, res) => {
   res.render('index', data)
 })
+
+app.get('/nojs', (req, res) => {
+  res.render('nojs', data)
+})
+
+function routeCards() {
+  for (let i = 0; i < data.cards.length; i++) {
+    app.get('/cards/' + data.cards[i].id, (req, res) => {
+      let singlecard = data.cards[i]
+      res.render('singlecard', { data: singlecard })
+    })
+  }
+}
 
 app.listen(port, () => {
   console.log('listing on port: ' + port)
